70b5fdf2fd2eeed3db39aab77c778d0e5a858860
Ayley <larshatzky@gmail.com>
2024-03-15 22:49:35 +0100

    Added: - CacheFileUtils (File utils in cache directory) - CachedNetworkImage own interpretation

---
+++ flutter_master_cache/lib/util/cache_file_utils.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class CacheFileUtils {
+  static Future<File> getFile(String filename) async {
+    final dir = (await getApplicationCacheDirectory()).path;
+    return File('$dir/$filename');
+  }
+
+  static Future<File> createFile(String filename,
+      {bool recursiv = false}) async {
+    final file = await getFile(filename);
+    return await file.create(recursive: recursiv);
+  }
+
+  static Future<bool> existFile(String filename) async {
+    final file = await getFile(filename);
+    return file.existsSync();
+  }
+
+  static Future<File> writeFileBytes(
+    String filename,
+    List<int> content, {
+    bool recursive = false,
+  }) async {
+    final file = await getFile(filename);
+    await file.create(recursive: recursive);
+    final raf = file.openSync(mode: FileMode.write);
+    await raf.writeFrom(content);
+    await raf.close();
+
+    return file;
+  }
+
+  static Future<File> writeFileString(
+    String filename,
+    String content, {
+    bool recursive = false,
+  }) async {
+    final file = await getFile(filename);
+    await file.create(recursive: recursive);
+    final raf = file.openSync(mode: FileMode.write)..writeStringSync(content);
+    await raf.close();
+
+    return file;
+  }
+
+  static Future<List<int>> readFileBytes(String filename) async {
+    final content = <int>[];
+    final file = await getFile(filename);
+    final raf = file.openSync()..readIntoSync(content);
+    await raf.close();
+
+    return content;
+  }
+
+  static Future<String> readFileString(String filename) async {
+    final content = await readFileBytes(filename);
+
+    return String.fromCharCodes(content);
+  }
+
+  static Future<void> deleteFile(String filename) async {
+    final file = await getFile(filename);
+    await file.delete();
+  }
+
+  static Future<void> deleteCache() async {
+    final dir = await getApplicationCacheDirectory();
+    await dir.delete();
+  }
+}
---
+++ flutter_master_cache/lib/widgets/cached_network_image.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_master_cache/util/cache_file_utils.dart';
+import 'package:http/http.dart' as http;
+
+class CachedNetworkImage extends StatelessWidget {
+
+  CachedNetworkImage({
+    required this.url,
+    this.name,
+    this.fit,
+    this.size,
+    this.loadingWidget,
+    this.errorBuilder,
+    this.frameBuilder,
+    this.scale = 1.0,
+    super.key,
+  });
+
+  final String url;
+  final String? name;
+  final BoxFit? fit;
+  final Size? size;
+  final double scale;
+
+  final Widget? loadingWidget;
+  final ImageErrorWidgetBuilder? errorBuilder;
+  final ImageFrameBuilder? frameBuilder;
+
+  final String _dir = 'images/';
+
+  File? _file;
+
+  Future<File> _downloadImage(String filename) async {
+    final res = await http.get(Uri.parse(url));
+
+    return CacheFileUtils.writeFileBytes(
+      filename,
+      res.bodyBytes,
+      recursive: true,
+    );
+  }
+
+  Future<File> _load() async {
+    if (_file != null) {
+      return _file!;
+    }
+
+    final filename = name ?? url.hashCode.toString();
+
+    if (await CacheFileUtils.existFile(_dir + filename)) {
+      _file = await CacheFileUtils.getFile(_dir + filename);
+    }
+
+    _file ??= await _downloadImage(_dir + filename);
+
+    return _file!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _load(),
+      builder: (context, s) {
+        if (s.hasData) {
+          return Image.file(
+            s.data!,
+            fit: fit,
+            width: size?.width,
+            height: size?.height,
+            errorBuilder: errorBuilder,
+            frameBuilder: frameBuilder,
+            scale: scale,
+          );
+        } else if (s.hasError) {
+          return errorBuilder?.call(
+                context,
+                s.error!,
+                s.stackTrace,
+              ) ??
+              Container();
+        }
+
+        return loadingWidget ?? Container();
+      },
+    );
+  }
+}
